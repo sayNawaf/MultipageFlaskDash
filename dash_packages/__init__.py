ce09cfc04db6c2bbddbaf497342181dd861788ef
sayNawaf <saynawaf6399@gmail.com>
2021-07-08 12:53:04 +0530

    fg

--- dash_packages/__init__.py
+++ dash_packages/__init__.py
@@ -4,7 +4,8 @@ import dash_bootstrap_components as dbc
 
 server = Flask(__name__)
 
-app = dash.Dash(__name__, server = server, url_base_pathname='/dashboard/',external_stylesheets = [dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, server = server, url_base_pathname='/dashboard/',external_stylesheets = [dbc.themes.BOOTSTRAP], meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}])
 # app2 = dash.Dash(__name__, server = server, url_base_pathname='/dashboard2/',external_stylesheets = [dbc.themes.BOOTSTRAP])
 app.config['suppress_callback_exceptions'] = True
 server = app.server
